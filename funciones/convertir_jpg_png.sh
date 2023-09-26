2a3f95817fd1633d96ae40048d4416fdfdcc5e28
Jesus Galvez Vazquez <111477236+jesusgvqz@users.noreply.github.com>
2023-09-25 18:53:29 -0600

    script-funcional
    script completado

--- funciones/convertir_jpg_png.sh
+++ funciones/convertir_jpg_png.sh
@@ -1,26 +1,26 @@
 #!/usr/bin/env bash
-
-ruta="$1"
-# valida parámetrps
-test -d "$ruta" || { echo "Se espera recibir un directorio.";ayuda; exit 1; }
+# Jesus Heron Galvez Vazquez
 
 # crea una función de ayuda
 function ayuda() {
-	echo "Estructura: convertir_jpg_png.sh $ruta"
-	echo "La $ruta debe ser de un directorio."
+	echo "Estructura: convertir_jpg_png.sh \$ruta"
+	echo "La \$ruta debe ser de un directorio. Ejemplo: /tmp/folder"
 }
+
+ruta="$1"
+# valida parámetrps
+test -d "$ruta" || { echo "Se espera recibir un directorio.";ayuda ; exit 1; }
+
 # la funcionalidad de conversión debe estar en una función o varias
 # usa globbing para el recorrito del directorio
 # usa el comando convert para la conversión
 conversion() {
-	local ruta_dir="$1"
-	for archivo in "$ruta_dir"/*.jpg; do
-			#echo "$archivo"
+	for archivo in "$ruta"/*.jpg; do
 			convert "$archivo" "$archivo.png"
 	done
 }
 
-conversion "$1"
-echo "test"
+conversion "$ruta"
+echo "Las imagenes se han convertido a .png"
 
 
